1e41b01e1683488639d1ec0519e7bc286cd1bb73
Saeed Nemati <sn.1361@gmail.com>
2026-08-22 15:00:57 +0330

    committed by script

---
+++ panel/admin/breadcrumb.jsx
@@ -0,0 +1,8 @@
+import PartBreadcrumb from 'partBreadcrumb'
+
+export default props => <PartBreadcrumb
+    icon='cardMembership'
+    name='memberships'
+    title='membershipsMemberships'
+    {...props}
+/>
